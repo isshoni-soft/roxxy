028dbd57e5d36e41bcbbd3fdb81e6fc2b6b09b4b
Andrew Burr <me@dragovorn.com>
2021-10-26 10:49:04 -0400

    make more portable

--- logging.go
+++ logging.go
@@ -6,10 +6,13 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
+var loggers []Logger
 var logFileChannel *safe_channel.SafeStringChannel
+var logFileCore string
 
 var defaultLogger = NewLogger("logger", 16)
 var dateLayout = "01-02-2006|15:04:05"
@@ -20,6 +23,7 @@ type Logger struct {
 	loggerChannel *safe_channel.SafeStringChannel
 
 	prefix string
+	storageIndex int
 }
 
 func InitLogfile(filePrefix string, fileName string) {
@@ -27,6 +31,7 @@ func InitLogfile(filePrefix string, fileName string) {
 		return
 	}
 
+	logFileCore = fileName
 	logFileName = filepath.Join(filePrefix, fileName + logFileName)
 	logFileChannel = safe_channel.NewSafeStringChannel(5)
 
@@ -45,35 +50,54 @@ func NewLogger(prefix string, buffer int) *Logger {
 	result := new(Logger)
 	result.prefix =  "[" + time.Now().Format(dateLayout) +"]: " + prefix + "| "
 	result.loggerChannel = safe_channel.NewSafeStringChannel(buffer)
+	result.storageIndex = len(loggers)
+
+	loggers = append(loggers, *result)
 
 	go result.loggerTick()
 
 	return result
 }
 
-func (l Logger) loggerTick() {
-	l.loggerChannel.ForEach(func(str string) {
-		fmt.Println(str)
-
-		if logFileChannel != nil {
-			logFileChannel.Offer(str) // now that we've logged the line lets queue it for adding to logfile
-		}
-	})
+func (l *Logger) loggerTick() {
+	for str := range l.loggerChannel.Channel() {
+		safe_channel.RunMain(func() {
+			fmt.Println(str)
+		})
+	}
 }
 
-func (l Logger) Shutdown() {
+func (l *Logger) Shutdown() {
 	l.loggerChannel.WaitForClose()
+
+	length := len(loggers)
+
+	if length > 1 {
+		loggers = append(loggers[:l.storageIndex], loggers[l.storageIndex + 1])
+	} else {
+		loggers = []Logger{}
+	}
 }
 
-func Shutdown() {
-	logFileChannel.WaitForClose()
+func ShutdownLogging() {
+	for _, logger := range loggers {
+		if logger.Closed() {
+			continue
+		}
+
+		logger.Shutdown()
+	}
+
+	if logFileEnabled {
+		logFileChannel.WaitForClose()
+	}
 }
 
-func (l Logger) SetPrefix(str string) {
+func (l *Logger) SetPrefix(str string) {
 	l.prefix = str
 }
 
-func (l Logger) Format(str ...string) (result string) {
+func (l *Logger) Format(str ...string) (result string) {
 	result = l.prefix
 
 	for _, s := range str {
@@ -83,15 +107,20 @@ func (l Logger) Format(str ...string) (result string) {
 	return
 }
 
-func (l Logger) Log(str ...string) {
+func (l *Logger) Log(str ...string) {
+	fmt.Println("queue log: " + strings.Join(str, " "))
 	l.loggerChannel.Offer(l.Format(str...))
+}
+
+func (l *Logger) Closed() bool {
+	return l.loggerChannel.Closed()
 }
 
 func fixLogFileNameCollisions() {
 	num := 1
 
 	for _, err := os.Stat(logFileName); os.IsExist(err); {
-		logFileName = "Sakura-" + time.Now().Format(dateLayout) + "-" + strconv.FormatInt(int64(num), 10) + ".log"
+		logFileName = logFileCore + "-" + time.Now().Format(dateLayout) + "-" + strconv.FormatInt(int64(num), 10) + ".log"
 		num++
 	}
 }
